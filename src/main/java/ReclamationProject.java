7df547db64e896e137e6e8a4d9ea82efa9f4cb6f
Zachary Oldham <zacharyoldham@gmail.com>
2017-09-26 15:50:32 -0500

    I refactored the code to be readable, and improved the efficiency of the method.

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -8,19 +8,38 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+/**Class that is an example of how to NOT write code.
+ *
+ */
+public class ReclamationProject {
+
+    /**Method to find and return the largest matching substring in the two strings.
+     *
+     * @param firstString the first string to be parsed for a matching substring
+     * @param secondString the second string to be parsed for a matching substring
+     * @return the largest matching section of the two strings
+     */
+    static String matchingSection(final String firstString, final String secondString) {
+        String str1;
+        String str2;
+        String strResult = "";
+        //ensure that firstString is shorter
+        if (firstString.length() > secondString.length()) {
+            str1 = secondString;
+            str2 = firstString;
+        } else {
+            str1 = firstString;
+            str2 = secondString;
+        }
+        for (int i = 0; i < str1.length(); i++) {
+            for (int j = str1.length() - i; j > 0; j--) {
+                for (int k = 0; k < str2.length() - j; k++) {
+                    if (j > strResult.length() && str1.regionMatches(i,  str2,  k,  j)) {
+                        strResult = str1.substring(i, i + j);
+                    }
+                }
+            }
+        }
+        return strResult;
+    }
 }
